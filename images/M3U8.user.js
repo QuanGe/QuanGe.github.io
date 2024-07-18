6ff0fa0934dac609d97f71864f7cfd821f52df89
ruquan.z <ru.quan.zhang.8@gmail.com>
2024-07-19 07:41:05 +0800

    change m3u8 js

--- images/M3U8.user.js
+++ images/M3U8.user.js
@@ -4,9 +4,6 @@
 // @version     0.0.1
 // @description 请复制此代码，并修改，然后在油猴脚本管理中创建您的油猴脚本, 如果脚本影响了您的正常使用，您应该添加@exclude规则排除某些网站，以便更好的体验
 // @match       *://*/*
-// @exclude     *://*.google.*/*
-// @exclude     *://*.baidu.*/*
-// @exclude     *://*.bing.*/*
 // ==/UserScript==
  (function() {
     window.shouldAutoSave = GM.info.scriptHandler === "Zipym";
